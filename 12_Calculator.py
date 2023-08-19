246a073f2eb96966b12713ce27b607f7a6c87600
Vladimir <vlad_e@rambler.ru>
2023-08-20 02:33:33 +0300

    Add fiile 12_Calculator.py
    Создание файла программы калькулятора Python

---
+++ 12_Calculator.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+
+window = tk.Tk()
+window.geometry('300x200')
+window.title('Python калькулятор')
+
+window.mainloop()
+
+
+# def main(args):
+#     return 0
+
+# if __name__ == '__main__':
+#     import sys
+#       sys.exit(main(sys.args))
+

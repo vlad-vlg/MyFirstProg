f1fd28e146d9b3d77d66b5a597d98837590456ed
Vladimir <vlad_e@rambler.ru>
2023-08-21 02:12:06 +0300

    Добавление полей ввода

--- 12_Calculator.py
+++ 12_Calculator.py
@@ -1,3 +1,5 @@
+# Python калькулятор
+
 import tkinter as tk
 
 
@@ -5,13 +7,14 @@ window = tk.Tk()
 window.geometry('300x200')
 window.title('Python калькулятор')
 
+first = tk.Label(text='Первое число')
+first.grid(column=0, row=0, padx=10, pady=10)
+second = tk.Label(text='Второе число')
+second.grid(column=0, row=1)
+
+firstEntry = tk.Entry()
+firstEntry.grid(column=1, row=0)
+secondEntry = tk.Entry()
+secondEntry.grid(column=1, row=1)
+
 window.mainloop()
-
-
-# def main(args):
-#     return 0
-
-# if __name__ == '__main__':
-#     import sys
-#       sys.exit(main(sys.args))
-
